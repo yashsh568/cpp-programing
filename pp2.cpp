def2e07d8cc8e692e8e967d15f45d11cb42e71ee
yashsh568 <98091956+yashsh568@users.noreply.github.com>
2022-12-13 12:09:02 +0530

    i#	Untitled1.cpp

---
+++ pp2.cpp
@@ -0,0 +1,43 @@
+//JUMP STATEMENTS 
+
+
+using namespace std;
+#include <iostream>
+
+//for break statement
+//int main()
+//{
+//
+//    for (int i = 0; i <= 10; ++i)
+//    {
+//        if (i == 8)
+//        {
+//            if (true)
+//            {
+//            }
+//            if(true)
+//            {
+//            }
+//            break;
+//        }
+//        cout << i << endl;
+//    }
+//}
+
+
+
+
+
+//for continue statement
+int main()
+{
+
+    for (int i = 0; i <= 10; ++i)
+    {
+        if (i == 8)
+        {
+            continue;
+        }
+        cout << i << endl;
+    }
+}
