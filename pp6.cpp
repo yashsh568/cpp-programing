9083f7d4b6490758b59243de3b376b22a124f7ab
yashsh568 <98091956+yashsh568@users.noreply.github.com>
2022-12-13 11:30:47 +0530

    pp6 added

---
+++ pp6.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        cout << s;
+    }
+}
